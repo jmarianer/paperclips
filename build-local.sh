d901f9c09c80f1bbef159f52877a580e9af674eb
Joey Marianer <roie.marianer@gmail.com>
2017-11-13 11:36:13 -0800

    Include fonts

--- build-local.sh
+++ build-local.sh
@@ -1,7 +1,10 @@
-# build.sh will (should) work on any machine that has Docker installed. This file requires npm, lessc, elm and probably some other stuff I've forgotten.
+# build.sh (which hasn't been written yet) will work on any machine that has
+# Docker installed. This file requires npm, lessc, elm and probably some other
+# stuff I've forgotten.
 set -e
 mkdir -p out
 lessc style.less > out/style.css
 cp index.html out
 cp -R icons out
+cp -R fonts out
 elm make Main.elm --output out/paperclips.js
